6d904bc5aa2075a669b64e016ef073de1ecd1c7a
Hoàng Võ Minh <hoangvominh@Hoangs-Mac.local>
2020-06-09 07:37:46 -0700

    style: add basic UI of login, register and TabBar

---
+++ libraryManagement/libraryManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  libraryManagement
+//
+//  Created by Hoàng Võ Minh on 6/9/20.
+//  Copyright © 2020 HD team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ libraryManagement/libraryManagement/loginViewController.swift
@@ -0,0 +1,49 @@
+//
+//  loginViewController.swift
+//  libraryManagement
+//
+//  Created by Hoàng Võ Minh on 6/9/20.
+//  Copyright © 2020 HD team. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+    //---Outlet
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //---Action
+    @IBAction func loginButton(_ sender: Any) {
+        print("login 1")
+        let username = usernameTextField.text
+        let password = passwordTextField.text
+        if (Username == username && Password == password){
+            print("login 2")
+            let mainViewController = (storyboard?.instantiateViewController(identifier: "mainViewController")) as! UITabBarController 
+            self.present(mainViewController, animated: true, completion: nil)
+        }
+    }
+    @IBAction func forgetButton(_ sender: Any) {
+    }
+    //---Variable
+    var Username:String = "hoang"
+    var Password:String = "123456"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
