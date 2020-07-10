fd486b75b91e4bcd06d6e269c275d59ed82d9668
hacaoduy <hacaoduy161@gmail.com>
2020-07-10 10:55:01 -0700

    authentication for register

--- libraryManagement/libraryManagement/loginViewController.swift
+++ libraryManagement/libraryManagement/loginViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class loginViewController: UIViewController {
     //---Outlet
--- libraryManagement/libraryManagement/registerViewController.swift
+++ libraryManagement/libraryManagement/registerViewController.swift
@@ -9,8 +9,15 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import FirebaseStorage
 
-class registerViewController: UIViewController {
+let storage = Storage.storage()
+let storageRef = storage.reference(forURL: "gs://librarymanagement-bd9ab.appspot.com")
+
+class registerViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    //--Variable
+    var imgData: Data!
+    
     //---Outlet
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var birthdayTextField: UITextField!
@@ -22,14 +29,23 @@ class registerViewController: UIViewController {
     
     //---Action
     @IBAction func tapAvatar(_ sender: UITapGestureRecognizer) {
-        let alert: UIAlertController = UIAlertController(title: "Notice", message: "Choose your avatar from", preferredStyle: .alert)
+        let alert: UIAlertController = UIAlertController(title: "Notice", message: "Choose your avatar from:", preferredStyle: .alert)
         let galleryButton: UIAlertAction = UIAlertAction(title: "Gallery", style: .default) { (UIAlertAction) in
             let imgPicker = UIImagePickerController()
             imgPicker.sourceType = UIImagePickerController.SourceType.photoLibrary
             imgPicker.delegate = self
+            imgPicker.allowsEditing = false
+            self.present(imgPicker, animated: true, completion: nil)
         }
         let cameraButton: UIAlertAction = UIAlertAction(title: "Camera", style: .default) { (UIAlertAction) in
-            
+            if (UIImagePickerController.isSourceTypeAvailable(.camera))
+            {
+                let imgPicker = UIImagePickerController()
+                imgPicker.sourceType = UIImagePickerController.SourceType.camera
+                imgPicker.delegate = self
+                imgPicker.allowsEditing = false
+                self.present(imgPicker, animated: true, completion: nil)
+            }
         }
         
         alert.addAction(galleryButton)
@@ -43,16 +59,60 @@ class registerViewController: UIViewController {
         Auth.auth().createUser(withEmail: emailTextField.text!, password: password_1TextField.text!) { authResult, error in
             if (error == nil)
             {
+                Auth.auth().signIn(withEmail: self.emailTextField.text!, password: self.password_1TextField.text!) { [weak self] authResult, error in guard let strongSelf = self else { return
+                    }
+                    if (error == nil)
+                    {
+                        print("Login success!")
+                    }
+                }
                 
+                let avatarRef = storageRef.child("images/\(self.emailTextField.text!).jpg")
+                
+                let uploadTask = avatarRef.putData(self.imgData, metadata: nil) { (metadata, error) in
+                    guard let metadata = metadata else {
+                        if (error != nil)
+                        {
+                            let alert:UIAlertController=UIAlertController(title: "Warning", message: "Fail to upload avatar", preferredStyle: .alert)
+                            let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default, handler: nil)
+                            alert.addAction(okButton)
+                            self.present(alert, animated: true, completion: nil)
+                        }
+                        else
+                        {
+                            let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                            changeRequest?.displayName = self.nameTextField.text!
+                            changeRequest?.commitChanges { (error) in
+                                if (error == nil)
+                                {
+                                    print("Register success!")
+                                }
+                                else
+                                {
+                                    let alert:UIAlertController=UIAlertController(title: "Warning", message: "Fail to update profile", preferredStyle: .alert)
+                                    let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default, handler: nil)
+                                    alert.addAction(okButton)
+                                    self.present(alert, animated: true, completion: nil)
+                                }
+                            }
+                        }
+                        return
+                    }
+                }
+                
+                uploadTask.resume()
+
             }
             else
             {
-                
+                let alert:UIAlertController=UIAlertController(title: "Warning", message: "Fail to register", preferredStyle: .alert)
+                let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
             }
         }
     }
     
-    //---Variable
     override func viewDidLoad() {
         super.viewDidLoad()
         password_1TextField.isSecureTextEntry  = true
