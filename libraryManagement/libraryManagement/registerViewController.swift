bbb35e43949a88e4250ea0ee598037fea3d2e0d6
hacaoduy <hacaoduy161@gmail.com>
2020-07-09 23:10:05 -0700

    add: tapAvatar

--- libraryManagement/libraryManagement/registerViewController.swift
+++ libraryManagement/libraryManagement/registerViewController.swift
@@ -7,18 +7,51 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class registerViewController: UIViewController {
     //---Outlet
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var birthdayTextField: UITextField!
     @IBOutlet weak var genderTextField: UITextField!
-    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var password_1TextField: UITextField!
     @IBOutlet weak var password_2TextField: UITextField!
     @IBOutlet weak var avatarImage: UIImageView!
     
     //---Action
+    @IBAction func tapAvatar(_ sender: UITapGestureRecognizer) {
+        let alert: UIAlertController = UIAlertController(title: "Notice", message: "Choose your avatar from", preferredStyle: .alert)
+        let galleryButton: UIAlertAction = UIAlertAction(title: "Gallery", style: .default) { (UIAlertAction) in
+            let imgPicker = UIImagePickerController()
+            imgPicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            imgPicker.delegate = self
+        }
+        let cameraButton: UIAlertAction = UIAlertAction(title: "Camera", style: .default) { (UIAlertAction) in
+            
+        }
+        
+        alert.addAction(galleryButton)
+        alert.addAction(cameraButton)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func registerButton(_ sender: Any) {
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: password_1TextField.text!) { authResult, error in
+            if (error == nil)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+    }
+    
     //---Variable
     override func viewDidLoad() {
         super.viewDidLoad()
