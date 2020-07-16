1a23df93fccc6b46d367b55cca88bddd7a4e651e
hacaoduy <hacaoduy161@gmail.com>
2020-07-16 07:39:04 -0700

    login

--- libraryManagement/libraryManagement/loginViewController.swift
+++ libraryManagement/libraryManagement/loginViewController.swift
@@ -12,28 +12,47 @@ import FirebaseAuth
 
 class loginViewController: UIViewController {
     //---Outlet
-    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     //---Action
     @IBAction func loginButton(_ sender: Any) {
-        print("login 1")
-        let username = usernameTextField.text
-        let password = passwordTextField.text
-        if (Username == username && Password == password){
-            print("login 2")
-            let mainViewController = (storyboard?.instantiateViewController(identifier: "mainViewController")) as! UITabBarController 
-            self.present(mainViewController, animated: true, completion: nil)
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { [weak self] authResult, error in
+          guard let strongSelf = self else { return }
+            if (error == nil) {
+                print("Login success!")
+                self!.loggedIn()
+            }
+            else
+            {
+                let alert:UIAlertController=UIAlertController(title: "Warning", message: "Wrong email or password!", preferredStyle: .alert)
+                let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alert.addAction(okButton)
+                self!.present(alert, animated: true, completion: nil)
+            }
         }
+
     }
     @IBAction func forgetButton(_ sender: Any) {
     }
     //---Variable
     var Username:String = "hoang"
     var Password:String = "123456"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+          print ("Error signing out: %@", signOutError)
+        }
+        
         passwordTextField.isSecureTextEntry = true
+        
+        loggedIn()
         // Do any additional setup after loading the view.
     }
     
