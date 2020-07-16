fecc68f07bd5bea601c58e74b9cf382f06f3820b
HoangVoMinh1999 <hoangvm.hiephoa@gmail.com>
2020-07-15 23:43:27 -0700

    add : statistic of students

--- libraryManagement/libraryManagement/statisticStudentsViewController.swift
+++ libraryManagement/libraryManagement/statisticStudentsViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 
 class statisticStudentsViewController: UIViewController {
-
+    @IBOutlet weak var totalStudentLabel: UILabel!
+    @IBOutlet weak var availableStudentLabel: UILabel!
+    @IBOutlet weak var disabledStudentLabel: UILabel!
+    //---Variable
+    var temp:UserDefaults = UserDefaults()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var data:Array<Dictionary<String,Any>> = temp.value(forKey: "data_students") as! Array<Dictionary<String, Any>>
+        totalStudentLabel.text = "\(data.count)"
+        availableStudentLabel.text = "\(availableStudents(data: data))"
+        disabledStudentLabel.text = "\(data.count-availableStudents(data: data))"
+        
+        
         // Do any additional setup after loading the view.
     }
-    
+    func availableStudents(data:Array<Dictionary<String,Any>>) -> Int {
+        var count = 0
+        for i in data {
+            if (i["status"] as! Bool){
+                count += 1
+            }
+        }
+        return count
+    }
 
     /*
     // MARK: - Navigation
