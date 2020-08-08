3af544411456e932d256649b24202a66a2ac2a17
Hoang Vo Minh <hoangvm.hiephoa@gmail.com>
2020-08-08 00:59:31 -0700

    fix: add book

--- libraryManagement/libraryManagement/addBooksViewController.swift
+++ libraryManagement/libraryManagement/addBooksViewController.swift
@@ -33,33 +33,40 @@ class addBooksViewController: UIViewController {
     var temp = UserDefaults()
     //---Action---
     @IBAction func confirmButton(_ sender: Any) {
-        let new_book:Book = Book(ID: bookIDTextField.text!, name: booknameTextField.text!, category: categoryTextField.text!, author: authorTextField.text!, publishingyear: publishingyearTextField.text!,publishingcompany: publishingcompanyTextField.text!, dateadded: dateaddedTextField.text!, status: 1, quantity: Int(quantityTextField.text!)!, check: 0)
-        let ID_books:Array<String> = temp.value(forKey: "ID_books") as! Array<String>
-        
-        
-        if (ID_books.count == 1){
-            let db = Firestore.firestore()
-            db.collection("Books").document("\(ID_books[0])")
-            .addSnapshotListener { documentSnapshot, error in
-              guard let document = documentSnapshot else {
-                print("Error fetching document: \(error!)")
-                return
-              }
-              guard let data = document.data() else {
-                print("Document data was empty.")
-                return
-              }
-                if (data["name"] as! String == ""){
-                    new_book.updateDetail(ID: ID_books[0])
-                }
-                else{
-                    new_book.insertNewBook()
+        if (booknameTextField.text == "" || bookIDTextField.text == "" || categoryTextField.text == "" || authorTextField.text == "" || publishingcompanyTextField.text == "" || publishingyearTextField.text == "" || dateaddedTextField.text == "" || quantityTextField.text == ""){
+            let alert:UIAlertController = UIAlertController(title: "Notice", message: "Please fill all infomation !!!", preferredStyle: .alert)
+            let okButton:UIAlertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert,animated: true,completion: nil)
+        } else {
+            let new_book:Book = Book(ID: bookIDTextField.text!, name: booknameTextField.text!.uppercased(), category: categoryTextField.text!, author: authorTextField.text!, publishingyear: publishingyearTextField.text!,publishingcompany: publishingcompanyTextField.text!, dateadded: dateaddedTextField.text!, status: 1, quantity: Int(quantityTextField.text!)!, check: 0)
+            let ID_books:Array<String> = self.temp.value(forKey: "ID_books") as! Array<String>
+            for id in ID_books {
+                let db = Firestore.firestore()
+                db.collection("Books").document("\(id)")
+                .addSnapshotListener { documentSnapshot, error in
+                  guard let document = documentSnapshot else {
+                    print("Error fetching document: \(error!)")
+                    return
+                  }
+                  guard let data = document.data() else {
+                    print("Document data was empty.")
+                    return
+                  }
+                  if (data["name"] as! String == new_book.name){
+                    let alert:UIAlertController=UIAlertController(title: "Notice", message: "This book is existed", preferredStyle: .alert)
+                    let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alert.addAction(okButton)
+                    self.present(alert,animated: true,completion: nil)
+                    return
+                  } else if (data["name"] as! String == ""){
+                    new_book.updateDetail(ID: id)
+                    return
+                  }
                 }
             }
-        } else {
             new_book.insertNewBook()
         }
-        self.performSegue(withIdentifier: "unwindToMenuBookWithSegue", sender: self)
     }
     
     @IBAction func cancelButton(_ sender: Any) {
