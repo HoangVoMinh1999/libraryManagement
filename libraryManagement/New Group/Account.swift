d70b3c3a32f7e9a3d5eb90981354e2b69afa2794
hacaoduy <hacaoduy161@gmail.com>
2020-08-18 05:22:59 -0700

    update: registerVC(avatar, confirm button); add: class Account

---
+++ libraryManagement/New Group/Account.swift
@@ -0,0 +1,76 @@
+//
+//  Account.swift
+//  libraryManagement
+//
+//  Created by Ha Cao Duy on 17/8/20.
+//  Copyright © 2020 HD team. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Account{
+    var name:String
+    var birthday:String
+    var gender:String
+    var email:String
+    var password:String
+    var avatar:String
+    var status:Int
+    
+    init() {
+        name = ""
+        birthday = ""
+        gender = ""
+        email = ""
+        password = ""
+        avatar = ""
+        status = 0
+    }
+    
+    init(name:String,birthday:String,gender:String,email:String,password:String,avatar:String,status:Int) {
+        self.name = name
+        self.birthday = birthday
+        self.gender = gender
+        self.email = email
+        self.password = password
+        self.avatar = avatar
+        self.status = status
+    }
+    
+    func insertNewAccount(){
+        let db = Firestore.firestore()
+        var ref: DocumentReference? = nil
+
+        ref = db.collection("Accounts").addDocument(data: [
+            "name":"\(self.name)",
+            "birthday":"\(self.birthday)",
+            "gender":"\(self.gender)",
+            "email":"\(self.email)",
+            "avatar":"\(self.avatar)",
+            "status":"\(self.status)"
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+    }
+    
+    func updateDetail(ID:String){
+        let db = Firestore.firestore()
+        let ref: DocumentReference? = nil
+        db.collection("Accounts").document("\(ID)").setData([
+            "name":"\(self.name)",
+            "birthday":"\(self.birthday)",
+            "gender":"\(self.gender)",
+            "email":"\(self.email)",
+            "password":"\(self.password)",
+            "avatar":"\(self.avatar)",
+            "status":"\(self.status)"
+        ], merge: true)
+    }
+
+    
+}
--- libraryManagement/libraryManagement/registerViewController.swift
+++ libraryManagement/libraryManagement/registerViewController.swift
@@ -14,9 +14,26 @@ import FirebaseStorage
 let storage = Storage.storage()
 let storageRef = storage.reference(forURL: "gs://librarymanagement-bd9ab.appspot.com")
 
-class registerViewController: UIViewController {
+class registerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return list[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.genderTextField.text = self.list[row]
+    }
+    
     //--Variable
     var imgData: Data!
+    var list = ["Male","Female","Other"]
     
     //---Outlet
     @IBOutlet weak var nameTextField: UITextField!
@@ -28,6 +45,40 @@ class registerViewController: UIViewController {
     @IBOutlet weak var avatarImage: UIImageView!
     
     //---Action
+    @IBAction func birthdayAction(_ sender: Any) {
+        let alert = UIAlertController(title: "\n\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+        let datePicker = UIDatePicker(frame: CGRect(x: 20, y:0, width: 400, height: 300))
+        datePicker.sizeToFit()
+        datePicker.datePickerMode = .date
+        alert.view.addSubview(datePicker)
+        let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+            self.birthdayTextField.text = datePicker.date.dateToString()
+        }
+        alert.addAction(okButton)
+        let cancelButton:UIAlertAction=UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alert.addAction(cancelButton)
+        //        alert.show()
+        self.present(alert,animated:true,completion: nil)
+    }
+    
+    @IBAction func genderAction(_ sender: Any) {
+        genderTextField.text = "Male"
+        let alert=UIAlertController(title: "\n\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+        let picker = UIPickerView(frame: CGRect(x: 30, y:0, width: 300, height: 400))
+        picker.dataSource=self
+        picker.delegate=self
+        picker.sizeToFit()
+        alert.view.addSubview(picker)
+        let okButton:UIAlertAction = UIAlertAction(title: "OK", style: .destructive, handler: nil)
+        alert.addAction(okButton)
+        let cancelButton:UIAlertAction=UIAlertAction(title: "Cancel", style: .cancel) { (UIAlertAction) in
+            self.genderTextField.text=""
+        }
+        alert.addAction(cancelButton)
+        
+        present(alert,animated: true,completion: nil)
+    }
+    
     @IBAction func tapAvatar(_ sender: UITapGestureRecognizer) {
         let alert: UIAlertController = UIAlertController(title: "Notice", message: "Choose your avatar from:", preferredStyle: .alert)
         let galleryButton: UIAlertAction = UIAlertAction(title: "Gallery", style: .default) { (UIAlertAction) in
@@ -56,67 +107,45 @@ class registerViewController: UIViewController {
     
     
     @IBAction func registerButton(_ sender: Any) {
-        Auth.auth().createUser(withEmail: emailTextField.text!, password: password_1TextField.text!) { authResult, error in
-            if (error == nil)
-            {
-                Auth.auth().signIn(withEmail: self.emailTextField.text!, password: self.password_1TextField.text!) { [weak self] authResult, error in guard let strongSelf = self else { return
-                    }
-                    if (error == nil)
-                    {
-                        print("Login success!")
-                    }
-                }
-                
-                let avatarRef = storageRef.child("images/\(self.emailTextField.text!).jpg")
-                
-                let uploadTask = avatarRef.putData(self.imgData, metadata: nil) { (metadata, error) in
-                    guard let metadata = metadata else {
-                        if (error != nil)
-                        {
-                            let alert:UIAlertController=UIAlertController(title: "Warning", message: "Fail to upload avatar", preferredStyle: .alert)
-                            let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default, handler: nil)
-                            alert.addAction(okButton)
-                            self.present(alert, animated: true, completion: nil)
-                        }
-                        else
-                        {
-                            //let size = metadata.size
-                            avatarRef.downloadURL { (url, error) in
-                              guard let downloadURL = url else {
-                                let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-                                changeRequest?.displayName = self.nameTextField.text!
-                                changeRequest?.commitChanges { (error) in
-                                    if (error == nil)
-                                    {
-                                        print("Register success!")
-                                    }
-                                    else
-                                    {
-                                        let alert:UIAlertController=UIAlertController(title: "Warning", message: "Fail to update profile", preferredStyle: .alert)
-                                        let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default, handler: nil)
-                                        alert.addAction(okButton)
-                                        self.present(alert, animated: true, completion: nil)
-                                    }
-                                }
-                                return
-                              }
-                            }
-                        }
-                        return
-                    }
-                }
-                
-                uploadTask.resume()
-
-            }
-            else
-            {
-                let alert:UIAlertController=UIAlertController(title: "Warning", message: "Fail to register", preferredStyle: .alert)
+        if (nameTextField.text! == "" || birthdayTextField.text! == "" || genderTextField.text! == "" || emailTextField.text! == "" || password_1TextField.text! == "" || password_2TextField.text! == "") {
+            let alert:UIAlertController=UIAlertController(title: "Warning", message: "Please fill all information", preferredStyle: .alert)
+            let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+        else {
+            if (password_1TextField.text! != password_2TextField.text!) {
+                let alert:UIAlertController=UIAlertController(title: "Warning", message: "Password confirm must be the same as password", preferredStyle: .alert)
                 let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default, handler: nil)
                 alert.addAction(okButton)
                 self.present(alert, animated: true, completion: nil)
             }
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: password_1TextField.text!) { authResult, error in
+                if (error == nil)
+                {
+
+                    let db = Firestore.firestore()
+                    db.collection("Accounts").document(self.emailTextField.text!).setData(["name" : self.nameTextField.text,"birthday": self.birthdayTextField.text,"gender":self.genderTextField.text,"email": self.emailTextField.text,"avatar": "gs://librarymanagement-bd9ab.appspot.com/images/avatar.png"])
+                    
+
+                    let alert:UIAlertController=UIAlertController(title: "Success", message: error?.localizedDescription, preferredStyle: .alert)
+                    let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+                        self.updateData()
+                    }
+                    alert.addAction(okButton)
+                    self.present(alert, animated: true, completion: nil)
+                    
+                }
+                else
+                {
+                    let alert:UIAlertController=UIAlertController(title: "Warning", message: error?.localizedDescription, preferredStyle: .alert)
+                    let okButton:UIAlertAction=UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alert.addAction(okButton)
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
         }
+        
     }
     
     override func viewDidLoad() {
@@ -125,6 +154,7 @@ class registerViewController: UIViewController {
         password_2TextField.isSecureTextEntry  = true
         password_1TextField.textContentType = .newPassword
         password_2TextField.textContentType = .newPassword
+        dismiss(animated: true, completion: nil)
         
         // avatarImage.image = UIImage(contentsOfFile: "avatar")
         
@@ -136,6 +166,8 @@ extension registerViewController: UIImagePickerControllerDelegate & UINavigation
 {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let chooseImg = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+       
+        
         let imgValue = max(chooseImg.size
             .width, chooseImg.size.height)
         if (imgValue > 3000) {
@@ -150,5 +182,34 @@ extension registerViewController: UIImagePickerControllerDelegate & UINavigation
         avatarImage.image = UIImage(data: imgData)
         
         dismiss(animated: true, completion: nil)
+    }
+    
+    func updateData(){
+        let queue: DispatchQueue = DispatchQueue(label: "UpdateImage")
+        queue.async {
+            do{
+                let ref = Storage.storage().reference().child("images/\(self.emailTextField.text!)")
+                 let metaData = StorageMetadata()
+                let chooseImg = self.avatarImage.image
+                let pickerData = chooseImg!.jpegData(compressionQuality: 1)!
+                 metaData.contentType = "image/jpeg"
+                 
+                 ref.putData(pickerData, metadata: metaData){(metaData,err) in
+                     guard metaData != nil else {
+                         return
+                     }
+                     ref.downloadURL { (url, err) in
+                         guard url != nil else {
+                             return
+                         }
+                     }
+                 }
+            }
+            DispatchQueue.main.async {
+                let db = Firestore.firestore()
+                db.collection("Accounts").document("\(self.emailTextField.text!)").updateData(["avatar" : "gs://librarymanagement-bd9ab.appspot.com/images/\(self.emailTextField.text!)"])
+            }
+        }
+
     }
 }
